5624fc212844c6e2ee013c6add40bd453040bfaf
Felistas <felistaswaceera@gmail.com>
2017-02-07 00:45:40 +0300

    Adding final test cases

--- cases.py
+++ cases.py
@@ -1,3 +1,21 @@
 def case(a):
 	if type (a) is str:
 		return True
+	if type(a) is int:
+		return True
+	if type(a) is list:
+		return True
+	if type(a) is dict:
+		return True
+	if type(a) is float:
+		return True
+	if a%2==0:
+		return True
+	if a%2!=0:
+		return True
+	if a==0:
+		return True
+	if a>0:
+		return True
+	if a<0:
+		return True
--- test.py
+++ test.py
@@ -5,6 +5,23 @@ class Testcase (unittest.TestCase):
 		self.assertEqual(cases.case('example'), True)
 	def test_int(self):
 		self.assertEqual(cases.case(4), True)
+	def test_list(self):
+		self.assertEqual(cases.case(['a',2]), True)
+	def test_dict(self):
+		self.assertEqual(cases.case({'a':2 }), True)
+	def test_float(self):
+		self.assertEqual(cases.case(0.75), True)
+	def test_even(self):
+		self.assertEqual(cases.case(4), True)
+	def test_odd(self):
+		self.assertEqual(cases.case(9), True)
+	def test_zero(self):
+		self.assertEqual(cases.case(0), True)
+	def test_greaterthanzero(self):
+		self.assertEqual(cases.case(2), True)
+	def test_lessthanzero(self):
+		self.assertEqual(cases.case(-1), True)
+	
 
 if __name__ == '__main__':
 
