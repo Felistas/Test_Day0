2ca8eadc93c4ef0de7215e647e9004a7a7966e0d
Felistas <felistaswaceera@gmail.com>
2017-02-06 23:14:34 +0300

    Adds a function to test if number is an integer

--- test.py
+++ test.py
@@ -2,7 +2,9 @@ import unittest
 import cases
 class Testcase (unittest.TestCase):
 	def test_string(self):
-		self.assertEqual(cases.case('Number'), True)
+		self.assertEqual(cases.case('example'), True)
+	def test_int(self):
+		self.assertEqual(cases.case(4), True)
 
 if __name__ == '__main__':
 
